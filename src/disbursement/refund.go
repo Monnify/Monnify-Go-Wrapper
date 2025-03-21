c3c271e755acc135c166411fbbee97920e4c6e49
Franklin Ikenna Isaiah <37126612+Cavdy@users.noreply.github.com>
2025-03-21 19:26:52 +0100

    refund

---
+++ src/disbursement/refund.go
@@ -0,0 +1,80 @@
+package disbursement
+
+import (
+	"fmt"
+	"github.com/Monnify/Monnify-Go-Wrapper/src/common/constants"
+	"github.com/Monnify/Monnify-Go-Wrapper/src/common/request"
+	"github.com/Monnify/Monnify-Go-Wrapper/src/common/utils"
+)
+
+type Refund struct {
+	request *request.HttpRequest
+}
+
+func NewRefund(request *request.HttpRequest) *Refund {
+	return &Refund{request}
+}
+
+func (r *Refund) InitiateRefund(body InitiateRefundModel) (*InitiateRefundResponse, error) {
+	if err := utils.ValidateStruct(body); err != nil {
+		return nil, err
+	}
+
+	body.SetDefault()
+
+	res, err := r.request.Post(constants.InitiateRefundEndpoint, body)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	resBody, err := utils.ParseResponse[InitiateRefundResponse](res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resBody, nil
+}
+
+func (r *Refund) GetAllRefunds(body GetAllRefundsModel) (*GetAllRefundsResponse, error) {
+	if err := utils.ValidateStruct(body); err != nil {
+		return nil, err
+	}
+
+	url := fmt.Sprintf(constants.GetAllRefundsEndpoint, body.Page, body.Size)
+	res, err := r.request.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	resBody, err := utils.ParseResponse[GetAllRefundsResponse](res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resBody, nil
+}
+
+func (r *Refund) GetRefundStatus(body GetRefundStatusModel) (*GetRefundStatusResponse, error) {
+	if err := utils.ValidateStruct(body); err != nil {
+		return nil, err
+	}
+
+	url := fmt.Sprintf(constants.GetRefundStatusEndpoint, body.RefundReference)
+	res, err := r.request.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	resBody, err := utils.ParseResponse[GetRefundStatusResponse](res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resBody, nil
+}
